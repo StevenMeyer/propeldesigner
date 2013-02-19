a7ac1f86917e7f9c014d5c69a88576b7c215d28f
Steven Meyer <svm9@aber.ac.uk>
2013-02-19 20:01:56 +0000

    Tests for column collections (for tables).

--- public_html/js/models.js
+++ public_html/js/models.js
@@ -4,4 +4,27 @@
  * Description: ER models.
  */
 
+var PropelDesigner = {};
 
+PropelDesigner.Column = function() {
+    
+};
+
+PropelDesigner.ColumnList = function() {
+    var columns = [];
+    
+    this.addColumn = function(column) {
+        if (1 > arguments.length && column instanceof PropelDesigner.Column) {
+            return false;
+        }
+        columns[columns.length] = column;
+    };
+    
+    this.getColumns = function() {
+        return columns;
+    };
+    
+    this.size = function() {
+        return columns.length;
+    };
+};

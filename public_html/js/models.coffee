dae30d51ad72419813f3ed024f01ba4efc32d839
Steven Meyer <svm9@aber.ac.uk>
2013-02-22 13:39:30 +0000

    Re-structured Column's CoffeeScript for better encapsulation.
    Tests are not all working; this commit is mainly to change dev machine.

--- public_html/js/models.coffee
+++ public_html/js/models.coffee
@@ -8,18 +8,187 @@
 ###
 
 class Column
+  @Inheritance =
+    FALSE:   false
+    SINGLE:  "single"
+    toArray: () ->
+      this[type] for type of this when type isnt "toArray"
+      
+  @PhpNamingMethod =
+    NOCHANGE:   "nochange"
+    UNDERSCORE: "underscore"
+    PHPNAME:    "phpname"
+    toArray:    () ->
+      this[method] for method of this when method isnt "toArray"
+      
+  @PropelType = 
+    ARRAY:       "ARRAY"
+    BLOB:        "BLOB"
+    BIGINT:      "BIGINT"
+    BOOLEAN:     "BOOLEAN"
+    CHAR:        "CHAR"
+    CLOB:        "CLOB"
+    DATE:        "DATE"
+    DECIMAL:     "DECIMAL"
+    DOUBLE:      "DOUBLE"
+    ENUM:        "ENUM"
+    FLOAT:       "FLOAT"
+    INTEGER:     "INTEGER"
+    LONGVARCHAR: "LONGVARCHAR"
+    OBJECT:      "OBJECT"
+    REAL:        "REAL"
+    SMALLINT:    "SMALLINT"
+    TIME:        "TIME"
+    TIMESTAMP:   "TIMESTAMP"
+    TINYINT:     "TINYINT"
+    VARCHAR:     "VARCHAR"
+    toArray:     () ->
+      this[type] for type of this when type isnt "toArray"
+      
+  constructor: (columnName) ->
+    _autoIncrement = false
+    _defaultExpr = null
+    _defaultValue = null
+    _description = null
+    _inheritance = Column.Inheritance.FALSE
+    _lazyLoad = false
+    _name = null
+    _peerName = null
+    _phpName = null
+    _phpNamingMethod = null
+    _phpType = null
+    _primaryKey = false
+    _primaryString = false
+    _required = false
+    _scale = null
+    _size = null
+    _sqlType = null
+    _type = Column.PropelType.VARCHAR
+    _valueSet = []
+    
+    if not String::trim
+      String::trim = () ->
+        this.replace(/^\s+|\s+$/g, '')
+    
+    @getDefaultExpr = () -> _defaultExpr
+      
+    @getDefaultValue = () -> _defaultValue
+      
+    @getDescription = () -> _description
+      
+    @getInheritance = () -> _inheritance
+      
+    @getName = () -> _name
+      
+    @getPeerName = () -> _peerName
+      
+    @getPhpName = () -> _phpName
+      
+    @getPhpNamingMethod = () -> _phpNamingMethod
+      
+    @getPhpType = () -> _phpType
+      
+    @getScale = () -> _scale
+      
+    @getSize = () -> _size
+      
+    @getSqlType = () -> _sqlType
+      
+    @getType = () -> _type
+    
+    @getValueSet = () -> _valueSet
+    
+    @isAutoIncrement = () -> _autoIncrement
+    
+    @isLazyLoad = () -> _lazyLoad
+    
+    @isPrimaryKey = () -> _primaryKey
+    
+    @isPrimaryString = () -> _primaryString
+    
+    @isRequired = () -> _required
+    
+    @setAutoIncrement = (bool = true) ->
+      _autoIncrement = if bool then true else false
+    
+    @setDefaultExpr = (expr) ->
+      _defaultExpr = if expr then expr else null
+      
+    @setDefaultValue = (value) ->
+      _defaultValue = if value then value else null
+      
+    @setDescription = (description) -> 
+      _description = if description then description else null
+      
+    @setInheritance = (type) ->
+      _inheritance = if type in Column.Inheritance.toArray() then type else Column.Inheritance.FALSE
+    
+    @setLazyLoad = (bool = true) ->
+      _lazyLoad = if bool then true else false
+      
+    @setName = (name) ->
+      name = name.trim() if typeof name is "string"
+      _name = if name then name else throw "Column must have a name"
+      
+    @setPeerName = (name) ->
+      name = name.trim() if typeof name is "string"
+      _peerName = if name then name else null
+      
+    @setPhpName = (name) ->
+      name = name.trim() if typeof name is "string"
+      _phpName = if name then name else null
+      
+    @setPhpNamingMethod = (method) ->
+      _phpNamingMethod = if method in Column.PhpNamingMethod.toArray() then method else null
+      
+    @setPhpType = (type) ->
+      type = type.trim() if typeof type is "string"
+      _phpType = if type then type else null
+      
+    @setPrimaryKey = (bool = true) ->
+      _primaryKey = if bool then true else false
+      
+    @setPrimaryString = (bool = true) ->
+      _primaryString = if bool then true else false
+      
+    @setRequired = (bool = true) ->
+      _required = if bool then true else false
+      
+    @setScale = (digits) ->
+      digits = parseInt digits
+      _scale = if not isNan digits then digits else null
+      
+    @setSize = (size) ->
+      size = parseInt size
+      _size = if not isNan size then size else null
+      
+    @setSqlType = (type) ->
+      type = type.trim() if typeof type is "string"
+      _sqlType = if type then type else null
+      
+    @setType = (type = @PropelType.VARCHAR) ->
+      _type = if type in Column.PropelType.toArray then type else Column.PropelType.VARCHAR
+    
+    @setValueSet = (array) ->
+      if array
+        if not array instanceof Array then array = [ array ]
+        _valueSet = array
+      else
+        _valueSet = []
+        
+    @setName columnName
 
 class ColumnList
   constructor: () ->
-    @columns = []
+    columns = []
     
-  addColumn: (column) ->
-    false if not column instanceof Column
-    @columns[@size()] = column
+    @addColumn = (column) ->
+      false if not column instanceof Column
+      @columns[@size()] = column
     
-  getColumns: () ->
-    @columns
+    @getColumns = () ->
+      @columns
       
   size: () ->
-    @columns.length
+    @getColumns.length
  
--- public_html/js/models.js
+++ public_html/js/models.js
@@ -7,11 +7,265 @@
  * THE JAVASCRIPT IS GENERATED FROM COFFEESCRIPT.
 */
 
-var Column, ColumnList;
+var Column, ColumnList,
+  __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
 Column = (function() {
 
-  function Column() {}
+  Column.Inheritance = {
+    FALSE: false,
+    SINGLE: "single",
+    toArray: function() {
+      var type, _results;
+      _results = [];
+      for (type in this) {
+        if (type !== "toArray") {
+          _results.push(this[type]);
+        }
+      }
+      return _results;
+    }
+  };
+
+  Column.PhpNamingMethod = {
+    NOCHANGE: "nochange",
+    UNDERSCORE: "underscore",
+    PHPNAME: "phpname",
+    toArray: function() {
+      var method, _results;
+      _results = [];
+      for (method in this) {
+        if (method !== "toArray") {
+          _results.push(this[method]);
+        }
+      }
+      return _results;
+    }
+  };
+
+  Column.PropelType = {
+    ARRAY: "ARRAY",
+    BLOB: "BLOB",
+    BIGINT: "BIGINT",
+    BOOLEAN: "BOOLEAN",
+    CHAR: "CHAR",
+    CLOB: "CLOB",
+    DATE: "DATE",
+    DECIMAL: "DECIMAL",
+    DOUBLE: "DOUBLE",
+    ENUM: "ENUM",
+    FLOAT: "FLOAT",
+    INTEGER: "INTEGER",
+    LONGVARCHAR: "LONGVARCHAR",
+    OBJECT: "OBJECT",
+    REAL: "REAL",
+    SMALLINT: "SMALLINT",
+    TIME: "TIME",
+    TIMESTAMP: "TIMESTAMP",
+    TINYINT: "TINYINT",
+    VARCHAR: "VARCHAR",
+    toArray: function() {
+      var type, _results;
+      _results = [];
+      for (type in this) {
+        if (type !== "toArray") {
+          _results.push(this[type]);
+        }
+      }
+      return _results;
+    }
+  };
+
+  function Column(columnName) {
+    var _autoIncrement, _defaultExpr, _defaultValue, _description, _inheritance, _lazyLoad, _name, _peerName, _phpName, _phpNamingMethod, _phpType, _primaryKey, _primaryString, _required, _scale, _size, _sqlType, _type, _valueSet;
+    _autoIncrement = false;
+    _defaultExpr = null;
+    _defaultValue = null;
+    _description = null;
+    _inheritance = Column.Inheritance.FALSE;
+    _lazyLoad = false;
+    _name = null;
+    _peerName = null;
+    _phpName = null;
+    _phpNamingMethod = null;
+    _phpType = null;
+    _primaryKey = false;
+    _primaryString = false;
+    _required = false;
+    _scale = null;
+    _size = null;
+    _sqlType = null;
+    _type = Column.PropelType.VARCHAR;
+    _valueSet = [];
+    if (!String.prototype.trim) {
+      String.prototype.trim = function() {
+        return this.replace(/^\s+|\s+$/g, '');
+      };
+    }
+    this.getDefaultExpr = function() {
+      return _defaultExpr;
+    };
+    this.getDefaultValue = function() {
+      return _defaultValue;
+    };
+    this.getDescription = function() {
+      return _description;
+    };
+    this.getInheritance = function() {
+      return _inheritance;
+    };
+    this.getName = function() {
+      return _name;
+    };
+    this.getPeerName = function() {
+      return _peerName;
+    };
+    this.getPhpName = function() {
+      return _phpName;
+    };
+    this.getPhpNamingMethod = function() {
+      return _phpNamingMethod;
+    };
+    this.getPhpType = function() {
+      return _phpType;
+    };
+    this.getScale = function() {
+      return _scale;
+    };
+    this.getSize = function() {
+      return _size;
+    };
+    this.getSqlType = function() {
+      return _sqlType;
+    };
+    this.getType = function() {
+      return _type;
+    };
+    this.getValueSet = function() {
+      return _valueSet;
+    };
+    this.isAutoIncrement = function() {
+      return _autoIncrement;
+    };
+    this.isLazyLoad = function() {
+      return _lazyLoad;
+    };
+    this.isPrimaryKey = function() {
+      return _primaryKey;
+    };
+    this.isPrimaryString = function() {
+      return _primaryString;
+    };
+    this.isRequired = function() {
+      return _required;
+    };
+    this.setAutoIncrement = function(bool) {
+      if (bool == null) {
+        bool = true;
+      }
+      return _autoIncrement = bool ? true : false;
+    };
+    this.setDefaultExpr = function(expr) {
+      return _defaultExpr = expr ? expr : null;
+    };
+    this.setDefaultValue = function(value) {
+      return _defaultValue = value ? value : null;
+    };
+    this.setDescription = function(description) {
+      return _description = description ? description : null;
+    };
+    this.setInheritance = function(type) {
+      return _inheritance = __indexOf.call(Column.Inheritance.toArray(), type) >= 0 ? type : Column.Inheritance.FALSE;
+    };
+    this.setLazyLoad = function(bool) {
+      if (bool == null) {
+        bool = true;
+      }
+      return _lazyLoad = bool ? true : false;
+    };
+    this.setName = function(name) {
+      if (typeof name === "string") {
+        name = name.trim();
+      }
+      return _name = (function() {
+        if (name) {
+          return name;
+        } else {
+          throw "Column must have a name";
+        }
+      })();
+    };
+    this.setPeerName = function(name) {
+      if (typeof name === "string") {
+        name = name.trim();
+      }
+      return _peerName = name ? name : null;
+    };
+    this.setPhpName = function(name) {
+      if (typeof name === "string") {
+        name = name.trim();
+      }
+      return _phpName = name ? name : null;
+    };
+    this.setPhpNamingMethod = function(method) {
+      return _phpNamingMethod = __indexOf.call(Column.PhpNamingMethod.toArray(), method) >= 0 ? method : null;
+    };
+    this.setPhpType = function(type) {
+      if (typeof type === "string") {
+        type = type.trim();
+      }
+      return _phpType = type ? type : null;
+    };
+    this.setPrimaryKey = function(bool) {
+      if (bool == null) {
+        bool = true;
+      }
+      return _primaryKey = bool ? true : false;
+    };
+    this.setPrimaryString = function(bool) {
+      if (bool == null) {
+        bool = true;
+      }
+      return _primaryString = bool ? true : false;
+    };
+    this.setRequired = function(bool) {
+      if (bool == null) {
+        bool = true;
+      }
+      return _required = bool ? true : false;
+    };
+    this.setScale = function(digits) {
+      digits = parseInt(digits);
+      return _scale = !isNan(digits) ? digits : null;
+    };
+    this.setSize = function(size) {
+      size = parseInt(size);
+      return _size = !isNan(size) ? size : null;
+    };
+    this.setSqlType = function(type) {
+      if (typeof type === "string") {
+        type = type.trim();
+      }
+      return _sqlType = type ? type : null;
+    };
+    this.setType = function(type) {
+      if (type == null) {
+        type = this.PropelType.VARCHAR;
+      }
+      return _type = __indexOf.call(Column.PropelType.toArray, type) >= 0 ? type : Column.PropelType.VARCHAR;
+    };
+    this.setValueSet = function(array) {
+      if (array) {
+        if (!array instanceof Array) {
+          array = [array];
+        }
+        return _valueSet = array;
+      } else {
+        return _valueSet = [];
+      }
+    };
+    this.setName(columnName);
+  }
 
   return Column;
 
@@ -20,22 +274,21 @@ Column = (function() {
 ColumnList = (function() {
 
   function ColumnList() {
-    this.columns = [];
+    var columns;
+    columns = [];
+    this.addColumn = function(column) {
+      if (!column instanceof Column) {
+        false;
+      }
+      return this.columns[this.size()] = column;
+    };
+    this.getColumns = function() {
+      return this.columns;
+    };
   }
 
-  ColumnList.prototype.addColumn = function(column) {
-    if (!column instanceof Column) {
-      false;
-    }
-    return this.columns[this.size()] = column;
-  };
-
-  ColumnList.prototype.getColumns = function() {
-    return this.columns;
-  };
-
   ColumnList.prototype.size = function() {
-    return this.columns.length;
+    return this.getColumns.length;
   };
 
   return ColumnList;

5f4fbf5ec68ad565e9660919fbb2106c2f5e6911
Steven Meyer <svm9@aber.ac.uk>
2013-03-12 19:35:34 +0000

    Simple XML loader and tests

---
+++ public_html/js/builder.coffee
@@ -0,0 +1,37 @@
+###
+ *      Author: Steven Meyer <svm9@aber.ac.uk>
+ *        File: builder.coffee
+ * Description: Generates a JS model from Propel XML schemata and vice versa.
+ *
+ * DO NOT EDIT THE JAVASCRIPT .js FILE DIRECTLY.
+ * THE JAVASCRIPT IS GENERATED FROM COFFEESCRIPT.
+###
+
+#jQuery ($) ->
+class Builder
+  @buildFromXML = (xml) ->
+    build = (xml) ->
+      _xml = $ $.parseXML xml
+      $database = _xml.find "database:first"
+      if $database.length is 0 then $.error "No database element found"
+      try
+        database = new Database ($database.attr "name"), ($database.attr "defaultIdMethod")
+      catch ex
+        $.error "The database element is malformed (#{ex})"
+      $database.children("table").each () ->
+        buildTable ($ this), database
+      database
+      
+    buildColumn = ($column, table) ->
+      column = new Column $column.attr "name"
+      table.addColumn column
+      table
+
+    buildTable = ($table, database) ->
+      table = new Table $table.attr "name"
+      database.addTable table
+      $table.children("column").each () ->
+        buildColumn ($ this), table
+      database
+
+    build xml
---
+++ public_html/js/builder.js
@@ -0,0 +1,55 @@
+/*
+ *      Author: Steven Meyer <svm9@aber.ac.uk>
+ *        File: builder.coffee
+ * Description: Generates a JS model from Propel XML schemata and vice versa.
+ *
+ * DO NOT EDIT THE JAVASCRIPT .js FILE DIRECTLY.
+ * THE JAVASCRIPT IS GENERATED FROM COFFEESCRIPT.
+*/
+
+var Builder;
+
+Builder = (function() {
+
+  function Builder() {}
+
+  Builder.buildFromXML = function(xml) {
+    var build, buildColumn, buildTable;
+    build = function(xml) {
+      var $database, database, _xml;
+      _xml = $($.parseXML(xml));
+      $database = _xml.find("database:first");
+      if ($database.length === 0) {
+        $.error("No database element found");
+      }
+      try {
+        database = new Database($database.attr("name"), $database.attr("defaultIdMethod"));
+      } catch (ex) {
+        $.error("The database element is malformed (" + ex + ")");
+      }
+      $database.children("table").each(function() {
+        return buildTable($(this), database);
+      });
+      return database;
+    };
+    buildColumn = function($column, table) {
+      var column;
+      column = new Column($column.attr("name"));
+      table.addColumn(column);
+      return table;
+    };
+    buildTable = function($table, database) {
+      var table;
+      table = new Table($table.attr("name"));
+      database.addTable(table);
+      $table.children("column").each(function() {
+        return buildColumn($(this), table);
+      });
+      return database;
+    };
+    return build(xml);
+  };
+
+  return Builder;
+
+})();
